6f3d6354f5088c53e55ebcbafc01fc53355e1e3d
Aidan McAlister <aidankmcalister@gmail.com>
2026-05-13 18:28:13 -0400

    remove committed build output, add .vercel to gitignore
    Vercel was deploying a stale pre-built .vercel/output from git instead
    of running a fresh build. This caused static assets (images, updated
    env vars) to never reflect actual source changes. Vercel will now build
    from source on every push.

--- .vercel/output/functions/__server.func/_chunks/_libs/@takumi-rs/image-response.mjs
+++
@@ -1,8 +0,0 @@
-import { I as ImageResponse } from "../../../_libs/takumi-js.mjs";
-import { c } from "../../../_libs/takumi-js.mjs";
-import "./helpers.mjs";
-export {
-  ImageResponse,
-  c as createImageResponse,
-  ImageResponse as default
-};
--- .vercel/output/static/assets/blog-DhQonDwc.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./main-C7iWp78g.js";function r(){return t.jsx(o,{})}export{r as component};
